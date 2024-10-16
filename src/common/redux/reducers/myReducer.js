a6e212ff684847d765a02a1c8a5b13d751c27521
Pratik <pratik080.rejoice@gmail.com>
2024-10-16 17:55:24 +0530

    finish: redux integration

--- src/common/redux/reducers/myReducer.js
+++ src/common/redux/reducers/myReducer.js
@@ -1,23 +1,49 @@
 import { createAsyncThunk, createSlice, current } from "@reduxjs/toolkit";
+import { ApiInstance } from "../../utils/api";
 
-// createAsyncThunk
+export const getProducts = createAsyncThunk(
+  'my/getProducts',
+  async (payload, thunkAPI) => {
+    try {
+      let res = await ApiInstance('/prducts')
+      return res
+    } catch (error) {
+      return thunkAPI.rejectWithValue('some custom error message')
+    }
+  }
+)
 
 const mySlice = createSlice({
   name: "my",
   initialState: {
     count: 0,
     data: [],
+    products: {},
+    loading: 'init',
+    error: ''
   },
   reducers: {
     increment: (state, action) => {
-      console.log("action**", action);
       state.count += action.payload.number;
     },
     decrement: (state, action) => {
       state.count -= action.payload.number;
     },
   },
-  extraReducers: () => {},
+  extraReducers: builder => {
+    builder
+      .addCase(getProducts.pending, (state, action) => {
+        state.loading = 'loading'
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.products = action.payload.data
+        state.loading = 'fulfilled'
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.loading = 'rejected'
+        state.error = action.payload
+      })
+  },
 });
 
 export default mySlice.reducer;
--- src/modules/dashboard/index.js
+++ src/modules/dashboard/index.js
@@ -1,33 +1,43 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { decrement, increment } from "../../common/redux/reducers/myReducer";
+import { decrement, getProducts, increment } from "../../common/redux/reducers/myReducer";
 import "./dashboard.css";
 
 function Dashboard() {
-  const data = useSelector((state) => state);
+  const data = useSelector((state) => state.my);
   const dispatch = useDispatch();
   console.log("data**", data);
 
+  useEffect(() => {
+    dispatch(getProducts('rrr'))
+  }, [])
+
   return (
     <div>
-      dashboard{" "}
-      <Link className="test" to={"/contactus"}>
-        To Contact us
-      </Link>
-      {data.my.count}
-      <button onClick={() => dispatch(increment({ number: 5 }))}>
-        Increment 5
-      </button>
-      <button onClick={() => dispatch(increment({ number: 10 }))}>
-        Increment 10
-      </button>
-      <button onClick={() => dispatch(decrement({ number: 2 }))}>
-        Decrement 2
-      </button>
-      <button onClick={() => dispatch(decrement({ number: 1 }))}>
-        Decrement 1
-      </button>
+      {
+        data.loading === 'loading' ?
+          <>loading.............</> :
+          <>
+            dashboard{" "}
+            <Link className="test" to={"/contactus"}>
+              To Contact us
+            </Link>
+            {data.count}
+            <button onClick={() => dispatch(increment({ number: 5 }))}>
+              Increment 5
+            </button>
+            <button onClick={() => dispatch(increment({ number: 10 }))}>
+              Increment 10
+            </button>
+            <button onClick={() => dispatch(decrement({ number: 2 }))}>
+              Decrement 2
+            </button>
+            <button onClick={() => dispatch(decrement({ number: 1 }))}>
+              Decrement 1
+            </button>
+          </>
+      }
     </div>
   );
 }
